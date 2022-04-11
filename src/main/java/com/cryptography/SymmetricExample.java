3f4ae45a21611ae39fd147e2e9fd48b583625a31
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-04-11 18:07:21 -0400

    create the encrypt function

--- src/main/java/com/cryptography/SymmetricExample.java
+++ src/main/java/com/cryptography/SymmetricExample.java
@@ -1,7 +1,11 @@
 package com.cryptography;
 
+import static java.lang.String.format;
+
 import com.cryptography.util.BytesToHex;
 import java.security.SecureRandom;
+import java.util.Base64;
+import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -10,7 +14,7 @@ public class SymmetricExample {
   private static final String ALGO = "AES";
   private static final String CIPHER = "AES/CBC/PKCS5PADDING";
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
 
     // generate a new key and initVrctor each time this runs. Real imple you would need to store the key and initvector as sec to later decrypt.
     SecureRandom sr = new SecureRandom();
@@ -24,13 +28,20 @@ public class SymmetricExample {
     final String payload = "This is the plaintext from Daedalus memoirs.";
     System.out.println("Original text = " + payload);
 
-//    encrypt()
+    final String encryptedPayload = encrypt(key, initVector, payload);
+    System.out.println(format("Encrypted text=%s", encryptedPayload));
+
+
   }
 
   public static String encrypt(byte[] key, byte[] initVector, String value) throws Exception {
     final IvParameterSpec ivParameterSpec = new IvParameterSpec(initVector);
     final SecretKeySpec secretKeySpec = new SecretKeySpec(key, ALGO);
-    //@TODO: Fill in more here.
-    return null;
+    final Cipher cipher = Cipher.getInstance(CIPHER);
+    cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+    final byte[] encrypted = cipher.doFinal(value.getBytes("UTF-8"));
+    final String encoded = Base64.getEncoder().encodeToString(encrypted);
+    return encoded;
   }
+
 }
